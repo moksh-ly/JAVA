301f0aadea02c56fbcfc03677e1d6cf743642def
moksh-ly <moksh0870@gmail.com>
2026-09-01 04:34:58 +0000

    swing1

---
+++ swingg/SwingDemo.java
@@ -0,0 +1,14 @@
+
+import java.swing.Jframe;
+import java.swing.JLabel;
+
+public class SwingDemo {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Swing Demo");
+        JLabel label = new JLabel("Hello, World!");
+        frame.add(label);
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
